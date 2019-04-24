64430f4f5a215e095f834bfd0d4d984cd9d67769
Abbey Carr <abbey.carr@uon.edu.au>
2019-04-24 04:46:19 +0000

    added rotation cipher with no key function

--- main.c
+++ main.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-/*Need to have 6 different function prototypes:
-Need to decide on data types and arguments*/
+//Need to have 6 different function prototypes for each task
 char encryptRotation(char* message, int key);
 char decryptRotation(char* message, int key);
 char encryptSubstitution(char* message, char* newalphabet, char* alphabet);
 char decryptSubstitution(char* message, char* newalphabet, char* alphabet);
-//char decryptRotationText();
-//char decryptSubstitutionText();
+char decryptRotationNoKey(char* message, int key);
+//char decryptSubstitutionNoKey();
 
 int main()  {
+    
 char message[100];
 char newalphabet[25];
 char alphabet[25]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 int key;    
+
 /* print a menu*/ 
 printf("Please select an option: \n");
 printf("a) Encrypt using rotation cipher \n");
 printf("b) Decrypt using rotation cipher \n");
 printf("c) Encrypt using substitution cipher \n");
 printf("d) Decrypt using substitution cipher \n");
-printf("e) Decrypt text only using rotation cipher \n");
-printf("f) Decrypt text only using substitution cipher \n");
+printf("e) Decrypt using rotation cipher without known key\n");
+printf("f) Decrypt using substitution cipher without known key\n");
 printf("Selection: ");
 char option;
 scanf ("%c",&option);
@@ -28,10 +29,10 @@ scanf ("%c",&option);
 switch (option)  {
     case 'a': encryptRotation(message, key); break; 
     case 'b': decryptRotation(message, key); break;
-   // case 'c': char encryptSubstitution(); break;
-   // case 'd': char decryptSubstitution(); break;
-   // case 'e': char decryptRotationText(); break;
-   // case 'f': char decryptSubstitutionText(); break;
+    case 'c': encryptSubstitution(message, newalphabet, alphabet); break;
+    case 'd': decryptSubstitution(message, newaplhabet, alphabet); break;
+    case 'e': decryptRotationNoKey(message, key); break;
+    //case 'f': char decryptSubstitutionNoKey(); break;
     default: printf("Unknown option %c\n Please enter a, b, c or d\n");
 }
   return 0;
@@ -168,10 +169,29 @@ char decryptSubstitution(char* message, char* newalphabet, char* alphabet)  {
     printf("Decrypted message: %s", message);
 	return 0;
 }
-
-//char decryptRotationText()  {
+//This function does not yet work correctly
+char decryptRotationNoKey(char* message, int key)  {
     /*This completes the same task as case b) but the key isn't given */ 
-//}
-//char decryptSubstitutionText()  {
+   char character;
+   int i;
+	
+	printf("Enter a message to decrypt: ");
+	scanf("%[^\n]*c",message);
+	
+	for(key =0; key<26; key++)  {
+    	for(i = 0; message[i] != '\0'; ++i)    {
+	    	character = message[i];
+		    character = character - key;
+			    if(character < 'A')  {
+				character = character + 'Z' - 'A' + 1;
+			    }
+			message[i] = character;
+		}
+	    printf("Key: %d Decrypted message: %s\n",key, message);
+	}
+	return 0;
+}
+
+//char decryptSubstitutionNoKey()  {
     /*This completes the same task as case d) but the key isn't given*/
 //} 
